7fb9f5b56112b79daeb0e85f26d22b46a111433e
biswajit2479 <biswajit.nayak@jifflenow.com>
2019-10-20 20:42:10 +0530

    fetching the matched words

--- lib/word_generator.rb
+++ lib/word_generator.rb
@@ -11,7 +11,9 @@ class NumberToWord
 					"8" => ["t", "u", "v"],
 					"9" => ["w", "x", "y", "z"]
 				}
-			
+		keys = number.split('').map{|num|letters[num]}
+		dictionary = load_dictonary
+		result = matced_words(dictionary, keys)
 	end
 
 	private
@@ -20,7 +22,34 @@ class NumberToWord
 		return true if number.empty? || number.length < 3  || number.split('').select{|num| (num.to_i == 0 || num.to_i == 1)}.count>0
 	end
 
+	def load_dictonary
+		dictionary = {}
+		for i in (1..30)
+      		dictionary[i] = []
+    	end
+	    File.foreach(Dir.pwd + '/data/dictionary.txt') do |word|
+	      dictionary[word.length] << word.chop.to_s.downcase
+	    end
+	    return dictionary
+	end
+
+	def matced_words(dictionary, keys)
+		results = {}
+    	total_number = keys.length - 1
+	    for i in (2..total_number - 2)
+	      first_array = keys[0..i]
+	      next if first_array.length < 3
+	      second_array = keys[i + 1..total_number]
+	      next if second_array.length < 3
+	      first_combination = first_array.shift.product(*first_array).map(&:join)
+	      next if first_combination.nil?
+	      second_combination = second_array.shift.product(*second_array).map(&:join)
+	      next if second_combination.nil?
+	      results[i] = [(first_combination & dictionary[i+2]), (second_combination & dictionary[total_number - i +1])]
+    	end
+    	return results
+	end
 end
 
-resultant_words = NumberToWord.new().convert_to_word("")
+resultant_words = NumberToWord.new().convert_to_word("6757632587")
 print resultant_words
